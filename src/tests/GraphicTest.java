4d2667d6578f1a9eb574ef5ceac6c22008279639
Mikkomario <mikko_hilpinen@hotmail.com>
2013-06-14 15:53:19 +0300

    Continued working with the tests

---
+++ src/tests/GraphicTest.java
@@ -0,0 +1,37 @@
+package tests;
+
+import handlers.ActorHandler;
+import handlers.DrawableHandler;
+import listeners.KeyListener;
+import listeners.MouseListener;
+
+/**
+ * Tests the graphical aspects of the program
+ *
+ * @author Gandalf.
+ *         Created 14.6.2013.
+ */
+public class GraphicTest extends AbstractTest
+{
+	/**
+	 * Creates the new test
+	 *
+	 * @param actorhandler
+	 * @param drawer
+	 * @param keylistener
+	 * @param mouselistener
+	 */
+	public GraphicTest(ActorHandler actorhandler, DrawableHandler drawer,
+			KeyListener keylistener, MouseListener mouselistener)
+	{
+		super(actorhandler, drawer, keylistener, mouselistener);
+	}
+
+	@Override
+	public void test()
+	{
+		// TODO Auto-generated method stub.
+		
+	}
+
+}
