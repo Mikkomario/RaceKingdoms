39118d3b64228d40c1554e3211a11283ac01dcec
Mikkomario <mikko_hilpinen@hotmail.com>
2013-06-14 15:00:23 +0300

    Started creating a visual test

---
+++ src/tests/GraphicsTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import processing.core.PApplet;
+import drawnobjects.DrawnObject2D;
+
+/**
+ * This class tests the graphical abilities of the program
+ *
+ * @author Gandalf.
+ *         Created 14.6.2013.
+ */
+public class GraphicsTest extends DrawnObject2D
+{
+	/**
+	 * Creates a new graphictest that tries to draw itself
+	 *
+	 * @param x
+	 * @param y
+	 */
+	public GraphicsTest(int x, int y)
+	{
+		super(x, y);
+		// TODO Auto-generated constructor stub.
+	}
+
+	@Override
+	public double getOriginX()
+	{
+		// TODO Auto-generated method stub.
+		return 0;
+	}
+
+	@Override
+	public double getOriginY()
+	{
+		// TODO Auto-generated method stub.
+		return 0;
+	}
+
+	@Override
+	public void drawSelfBasic(PApplet applet)
+	{
+		// TODO Auto-generated method stub.
+		
+	}
+}
