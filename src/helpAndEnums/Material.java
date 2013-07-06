90f0c46e6c07428f37b2cd2843bebd0246a03c1c
Mikkomario <mikko_hilpinen@hotmail.com>
2013-07-06 22:21:04 +0300

    Started creating the interactive collision system by creating material enumeration which is supposed to provide the density of the object

---
+++ src/helpAndEnums/Material.java
@@ -0,0 +1,17 @@
+package helpAndEnums;
+
+/**
+ * Material tells which material the object is made from. Each material has 
+ * its own density
+ *
+ * @author Gandalf.
+ *         Created 6.7.2013.
+ */
+public enum Material
+{
+	// See: http://en.wikipedia.org/wiki/Density#Water
+	// And http://www.avlandesign.com/density_metal.htm
+	WATER, ICE, WOOD, AIR, CORK, LITHIUM, ALUMINIUM, BERYLLIUMCOPPER, BRONZE, 
+	IRON, COPPER, COBALT, GOLD, PLATINUM, SILVER, STEEL, TIN, ALUMINUM, 
+	ALUMINUMBRONZE, DIAMOND, TITANIUM, LEAD, IRIDIUM;
+}
